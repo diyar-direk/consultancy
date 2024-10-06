b387230d022e15c7253fbd0a05e8425b7f350302
diyar-direk <diyardireki111@gmail.com>
2024-10-06 03:49:33 -0700

    Sa

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           </div>
         </div>
       </main>
-      <main className="home-services center">
+      <main className="home-services section-color center sub-page">
         <div className="container">
           <div className="center title flex-direction">
             <h1 className="title">our services</h1>
@@ -34,37 +34,114 @@ const Home = () => {
                 <i className="fa-solid fa-fire"></i>
               </ServicesComp.Icon>
               <ServicesComp.H1>Lorem ipsum dolor sit amet.</ServicesComp.H1>
-              <ServicesComp.P more={true}>
+              <ServicesComp.P>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
                 inventore odit, accusamus pariatur similique deserunt voluptate
-                quasi quibusdam excepturi unde quaerat minus commodi corrupti
-                asperiores non ex iusto ea neque.
+                quasi quibusdam
               </ServicesComp.P>
+              <ServicesComp.Details more={true} />
             </ServicesComp>
             <ServicesComp>
               <ServicesComp.Icon>
                 <i className="fa-solid fa-fire"></i>
               </ServicesComp.Icon>
               <ServicesComp.H1>Lorem ipsum dolor sit amet.</ServicesComp.H1>
-              <ServicesComp.P more={true}>
+              <ServicesComp.P>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
                 inventore odit, accusamus pariatur similique deserunt voluptate
-                quasi quibusdam excepturi unde quaerat minus commodi corrupti
-                asperiores non ex iusto ea neque.
+                quasi quibusdam
               </ServicesComp.P>
+              <ServicesComp.Details more={true} />
             </ServicesComp>
             <ServicesComp>
               <ServicesComp.Icon>
                 <i className="fa-solid fa-fire"></i>
               </ServicesComp.Icon>
               <ServicesComp.H1>Lorem ipsum dolor sit amet.</ServicesComp.H1>
-              <ServicesComp.P more={true}>
+              <ServicesComp.P>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
                 inventore odit, accusamus pariatur similique deserunt voluptate
-                quasi quibusdam excepturi unde quaerat minus commodi corrupti
-                asperiores non ex iusto ea neque.
+                quasi quibusdam
               </ServicesComp.P>
+              <ServicesComp.Details more={true} />
             </ServicesComp>
+          </div>
+          <Link className="btn2"> explorer our services </Link>
+        </div>
+      </main>
+      <main className="about-home">
+        <div className="flex">
+          <div className="image"></div>
+          <article className="center flex-direction">
+            <div className="center title flex-direction">
+              <h1 className="title">About Us</h1>
+              <h2> Lorem ipsum dolor sit amet. </h2>
+            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+              adipisci quia tempore
+            </p>
+            <div className="flex gap-20">
+              <Link className="btn2"> read more </Link>
+              <Link className="btn"> contact us </Link>
+            </div>
+          </article>
+        </div>
+      </main>
+      <main className="sub-page center flex-direction home-contact section-color">
+        <div className="center title flex-direction">
+          <h1 className="title">contact us</h1>
+          <h2> Lorem ipsum dolor sit amet. </h2>
+        </div>
+        <div className="container">
+          <div className="grid-2 align-start">
+            <div className="info">
+              <div>
+                <div className="center gap-20">
+                  <i className="fa-solid fa-map-location-dot"></i>
+                  <div className="flex-1">
+                    <h2> our location</h2>
+                    <a href="" target="_blank">
+                      syria alhasaka
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="center gap-20">
+                  <i className="fa-solid fa-envelope"></i>
+                  <div className="flex-1">
+                    <h2> our email</h2>
+                    <a href="" target="_blank">
+                      example@222.gmail.com
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="center gap-20">
+                  <i className="fa-solid fa-phone"></i>
+                  <div className="flex-1">
+                    <h2> our phone</h2>
+                    <a href="" target="_blank">
+                      +963 938 094
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="center gap-20">
+                <i className="fa-brands fa-facebook"></i>
+                <div className="flex-1">
+                  <h2> facebooke</h2>
+                  <a href="" target="_blank">
+                    +963 938 094
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="map">
+              <img src={require("./GoogleMapTA.webp")} alt="" />
+            </div>
           </div>
         </div>
       </main>
